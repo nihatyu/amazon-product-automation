106ff0aa976b87b0243c2bb4bce287fe5925c7ac
Nihat Uslu <nihatyusufuslu@gmail.com>
2023-07-14 18:22:09 +0300

    son çalışır hali

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function runAutomation() {
   const browser = await puppeteer.launch({
     headless: false,
     userDataDir: "Library/Application Support/mumku",
-    protocolTimeout: 1000000000,
+    protocolTimeout: 100000000,
     defaultViewport: null,
     args: [
       "--user-data-dir",
@@ -32,22 +32,41 @@ async function runAutomation() {
     }),
   ]);
 
+  await wait(1000);
+
   const timeout = 10000000;
   page1.setDefaultTimeout(timeout);
   page2.setDefaultTimeout(timeout);
 
   // Amazon linklerini içeren dizi
+  // Şablon ="'"&A1:A106&"',"
   const amazonLinks = [
-    "https://www.amazon.com/s?i=garden&bbn=289668&rh=n%3A1055398%2Cn%3A284507%2Cn%3A289668%2Cn%3A289826%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3Ak2eWXMKs%2FZiUo8%2BAUMW4uvvFqMloRXCSoh8WF8KUPOU&qid=1687626807&rnid=289668&ref=sr_nr_n_19",
-    "https://www.amazon.com/s?i=garden&bbn=289672&rh=n%3A1055398%2Cn%3A284507%2Cn%3A289668%2Cn%3A289672%2Cn%3A678652011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3AIj2w%2B7HjVK1Dt4plhhnE579ixTMK9vcUP135FT%2FsNrg&qid=1687626829&rnid=289672&ref=sr_nr_n_2",
-    "https://www.amazon.com/s?i=garden&bbn=289672&rh=n%3A1055398%2Cn%3A284507%2Cn%3A289668%2Cn%3A289672%2Cn%3A678651011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3AulcCt5%2BRoPxiAq%2BX72vM%2Brv%2BOXMpXV7ESMqffS3VErM&qid=1687626829&rnid=289672&ref=sr_nr_n_1",
+    "https://www.amazon.com/s?i=garden&bbn=5315091011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A5315091011%2Cn%3A18098892011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3A3i022PaoEC32993YmxZ5H2%2FlLxSh89%2BRxzujWkOpNz8&qid=1688253459&rnid=5315091011&ref=sr_nr_n_1",
+    "https://www.amazon.com/s?i=garden&bbn=18098916011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A5315091011%2Cn%3A18098916011%2Cn%3A18098919011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3AP2igpmSQtcXSq5LpO6fLoh%2BWv47n4g70aVDLuXDdjnw&qid=1688253461&rnid=18098916011&ref=sr_nr_n_2",
+    "https://www.amazon.com/s?i=garden&bbn=18098901011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A18098901011%2Cn%3A18098903011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3Az4JTcKdUwAOJpPJOX2upNK931icPNbKZkuGOvlJgKIM&qid=1688253549&rnid=18098901011&ref=sr_nr_n_1",
+    "https://www.amazon.com/s?i=garden&bbn=18098901011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A18098901011%2Cn%3A18098902011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3A111f005XAt%2F%2FD2%2B6Au4MfL5bvz8zVAIdCxjvVcb%2Bmcw&qid=1688253549&rnid=18098901011&ref=sr_nr_n_2",
+    "https://www.amazon.com/s?i=garden&bbn=18098901011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A18098901011%2Cn%3A18098904011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3AF5jy8Kii%2BGqsrO33V3CIJZHUcuoWVZ%2FFR9PKRI0J%2B%2BU&qid=1688253549&rnid=18098901011&ref=sr_nr_n_4",
+    "https://www.amazon.com/s?i=garden&bbn=5298059011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A5298059011%2Cn%3A5298060011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3A4veen7QXhzoWiGgUowVFGLN8J1uLw8iNytQn1ObHAEw&qid=1688253625&rnid=5298059011&ref=sr_nr_n_1",
+    "https://www.amazon.com/s?i=garden&bbn=5298068011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A5298059011%2Cn%3A5298068011%2Cn%3A5298069011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3ATjVfluJ4EOP6G5Ehz3AtH0L9aWh%2BL1IGI9uFe5YUGMU&qid=1688253633&rnid=5298068011&ref=sr_nr_n_1",
+    "https://www.amazon.com/s?i=garden&bbn=5298068011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A5298059011%2Cn%3A5298068011%2Cn%3A5298072011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3A4T6y0WpAExP7j4v6RJVPFuRIormWMedo1DcH35j6IW0&qid=1688253633&rnid=5298068011&ref=sr_nr_n_2",
+    "https://www.amazon.com/s?i=garden&bbn=5298068011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A5298059011%2Cn%3A5298068011%2Cn%3A5298073011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3ABRyx0ftqJWBG%2BXPLyjH78P2ZF5htiDbT6yeB7yleTJI&qid=1688253633&rnid=5298068011&ref=sr_nr_n_3",
+    "https://www.amazon.com/s?i=garden&bbn=5298068011&rh=n%3A1055398%2Cn%3A284507%2Cn%3A6054382011%2Cn%3A5298059011%2Cn%3A5298068011%2Cn%3A5298075011%2Cp_72%3A1248915011%2Cp_36%3A1500-7700&dc&ds=v1%3AQNHY5KEtTPubAMCPogOd76RkezOS9go%2ByWjecvQUISs&qid=1688253633&rnid=5298068011&ref=sr_nr_n_4",
   ];
 
   let totalInventory = 0;
   let pagesCanBeScanForFreeSpace = 0;
 
-  while (totalInventory < 49000) {
+  for (let i = 0; i < amazonLinks.length; i++) {
+    console.log("totalInventory :>> ", totalInventory);
+    console.log("pagesCanBeScanForFreeSpace :>> ", pagesCanBeScanForFreeSpace);
+    const index = i + 1; // Indeks bilgisi
+    //
+    //
+    // SAYFA SAYISINI ALDIĞIMIZ KISIM
+    const amazonLink = amazonLinks[i];
+
     await page2.reload();
+    console.log("reload çalıştı.");
     await wait(5000);
 
     await getInventoryTotal(page2);
@@ -61,73 +80,133 @@ async function runAutomation() {
       pagesCanBeScanForFreeSpace = pagesToScan;
     }
 
-    for (let i = 0; i < amazonLinks.length; i++) {
-      //
-      //
-      // SAYFA SAYISINI ALDIĞIMIZ KISIM
-      const amazonLink = amazonLinks[i];
+    let totalPages = 1;
 
-      let totalPages = 1;
+    await page1.goto(amazonLink, { waitUntil: "networkidle0" });
 
-      await page1.goto(amazonLink, { waitUntil: "networkidle0" });
+    await wait(2000);
 
-      await wait(2000);
+    await page1.waitForSelector("#sfButton > img");
+    await page1.click("#sfButton > img");
+    await wait(1000);
 
-      await page1.waitForSelector("#sfButton > img");
-      await page1.click("#sfButton > img");
+    await page1.waitForSelector("#sfClearFilters");
+    await page1.click("#sfClearFilters");
+    await wait(1000);
+
+    const selectorExists = await page1.evaluate(() => {
+      const selector = ".s-pagination-item.s-pagination-ellipsis";
+      return !!document.querySelector(selector);
+    });
+
+    if (selectorExists) {
+      const elements = await page1.$$eval(
+        ".s-pagination-item.s-pagination-disabled",
+        (items) => items.map((item) => Number(item.textContent))
+      );
+      if (elements.length >= 2) {
+        totalPages = elements[1];
+      }
+    } else {
+      const paginationItems = await page1.$$eval(
+        ".s-pagination-item",
+        (items) => items.length
+      );
+      if (paginationItems > 0) {
+        totalPages = paginationItems - 2;
+      }
+    }
+
+    //
+    //
+    // EXTENSİON ÜZERİNDEN SEARCH İŞLEMLERİNİ YAPTIĞIMIZ KISIM
+    await wait(2000);
+
+    let pageCountMin = 0;
+    let pageCountMax = pagesCanBeScanForFreeSpace;
+
+    await page1.waitForSelector("#sfStartSearch");
+    const startButton = await page1.$("#sfStartSearch");
+
+    if (pagesCanBeScanForFreeSpace >= totalPages) {
+      await page1.waitForSelector("#sfPageCountMin");
+      await page1.waitForSelector("#sfPageCountMax");
+      const sfPageCountMin = await page1.$("#sfPageCountMin");
+      const sfPageCountMax = await page1.$("#sfPageCountMax");
+
+      await sfPageCountMin.evaluate((input) => {
+        input.value = 0;
+      });
+      await sfPageCountMax.evaluate((input, totalPages) => {
+        input.value = totalPages;
+      }, totalPages);
       await wait(1000);
 
-      await page1.waitForSelector("#sfClearFilters");
-      await page1.click("#sfClearFilters");
+      // Start the search
+      await startButton.evaluate((button) => button.click());
       await wait(1000);
 
-      const selectorExists = await page1.evaluate(() => {
-        const selector = ".s-pagination-item.s-pagination-ellipsis";
-        return !!document.querySelector(selector);
+      await page1.waitForFunction(() => {
+        const button = document.querySelector("#sfSaveASINsButton");
+        return button && button.textContent === "ASIN'leri SellerFlash'a Aktar";
       });
 
-      if (selectorExists) {
-        const elements = await page1.$$eval(
-          ".s-pagination-item.s-pagination-disabled",
-          (items) => items.map((item) => Number(item.textContent))
-        );
-        if (elements.length >= 2) {
-          totalPages = elements[1];
-        }
-      } else {
-        const paginationItems = await page1.$$eval(
-          ".s-pagination-item",
-          (items) => items.length
-        );
-        if (paginationItems > 0) {
-          totalPages = paginationItems - 2;
-        }
-      }
+      // Click the button once it changes
+      await wait(1500);
+      await page1.click("#sfSaveASINsButton");
 
-      //
-      //
-      // EXTENSİON ÜZERİNDEN SEARCH İŞLEMLERİNİ YAPTIĞIMIZ KISIM
       await wait(2000);
 
-      let pageCountMin = 0;
-      let pageCountMax = pagesCanBeScanForFreeSpace;
+      const sfSavingInfo = await page1.$("#sfSavingInfo");
+      const sfSavingInfoText = await page1.evaluate(
+        (element) => element.textContent,
+        sfSavingInfo
+      );
 
-      await page1.waitForSelector("#sfStartSearch");
-      const startButton = await page1.$("#sfStartSearch");
+      if (sfSavingInfoText.includes("ASIN listesi kaydedilemedi.")) {
+        console.error(`ASIN listesi kaydedilemedi. ${amazonLinks[i]}`);
+      }
 
-      if (pagesCanBeScanForFreeSpace >= totalPages) {
+      await wait(2000);
+      //
+      //
+    } else {
+      if (totalInventory >= 49000) {
+        console.log(
+          "Total inventory is equal to or greater than 49000. waiting 15mins automation."
+        );
+        await wait(900000);
+        await page2.reload({ waitUntil: "networkidle0" });
+        await wait(2000);
+      }
+
+      let donguSayisi = Math.ceil(totalPages / pagesCanBeScanForFreeSpace);
+
+      for (let index = 0; index <= donguSayisi; index++) {
+        if (totalInventory >= 49000) {
+          console.log(
+            "Total inventory is equal to or greater than 49000. Stopping automation."
+          );
+          await wait(900000);
+          await page2.reload({ waitUntil: "networkidle0" });
+          await wait(2000);
+        }
+
         await page1.waitForSelector("#sfPageCountMin");
         await page1.waitForSelector("#sfPageCountMax");
         const sfPageCountMin = await page1.$("#sfPageCountMin");
         const sfPageCountMax = await page1.$("#sfPageCountMax");
 
-        await sfPageCountMin.evaluate((input) => {
-          input.value = 0;
-        });
-        await sfPageCountMax.evaluate((input, totalPages) => {
-          input.value = totalPages;
-        }, totalPages);
-        await wait(1000);
+        await sfPageCountMin.evaluate((input, pageCountMin) => {
+          input.value = pageCountMin;
+        }, pageCountMin);
+        await sfPageCountMax.evaluate((input, pageCountMax) => {
+          input.value = pageCountMax;
+        }, pageCountMax);
+        await wait(2000);
+
+        //
+        //
 
         // Start the search
         await startButton.evaluate((button) => button.click());
@@ -139,11 +218,10 @@ async function runAutomation() {
             button && button.textContent === "ASIN'leri SellerFlash'a Aktar"
           );
         });
-
         // Click the button once it changes
         await wait(1500);
-        await page1.click("#sfSaveASINsButton");
 
+        await page1.click("#sfSaveASINsButton");
         await wait(2000);
 
         const sfSavingInfo = await page1.$("#sfSavingInfo");
@@ -156,101 +234,38 @@ async function runAutomation() {
           console.error(`ASIN listesi kaydedilemedi. ${amazonLinks[i]}`);
         }
 
+        await wait(900000); // orjinal version da 900000 e çıkarılacak
+        await page2.reload({ waitUntil: "networkidle0" });
         await wait(2000);
-        //
-        //
-      } else {
-        if (totalInventory >= 49000) {
-          console.log(
-            "Total inventory is equal to or greater than 49000. waiting 15mins automation."
-          );
-          await wait(900000);
+
+        await getInventoryTotal(page2);
+
+        remainingProducts = 49000 - totalInventory;
+        pagesToScan = Math.ceil(remainingProducts / 27);
+        if (pagesToScan <= 3) {
           await page2.reload({ waitUntil: "networkidle0" });
-          await wait(2000);
+          await wait(3000);
+          continue;
         }
 
-        let donguSayisi = Math.ceil(totalPages / pagesCanBeScanForFreeSpace);
+        if (pagesToScan > 400) {
+          pagesCanBeScanForFreeSpace = 400;
+        } else {
+          pagesCanBeScanForFreeSpace = pagesToScan;
+        }
 
-        for (let index = 0; index <= donguSayisi; index++) {
-          if (totalInventory >= 49000) {
-            console.log(
-              "Total inventory is equal to or greater than 49000. Stopping automation."
-            );
-            await wait(900000);
-            await page2.reload({ waitUntil: "networkidle0" });
-            await wait(2000);
-          }
+        pageCountMin = pageCountMax + 1;
+        pageCountMax = pageCountMax + pagesCanBeScanForFreeSpace;
 
-          await page1.waitForSelector("#sfPageCountMin");
-          await page1.waitForSelector("#sfPageCountMax");
-          const sfPageCountMin = await page1.$("#sfPageCountMin");
-          const sfPageCountMax = await page1.$("#sfPageCountMax");
-
-          await sfPageCountMin.evaluate((input, pageCountMin) => {
-            input.value = pageCountMin;
-          }, pageCountMin);
-          await sfPageCountMax.evaluate((input, pageCountMax) => {
-            input.value = pageCountMax;
-          }, pageCountMax);
-          await wait(2000);
-
-          //
-          //
-
-          // Start the search
-          await startButton.evaluate((button) => button.click());
-          await wait(1000);
-
-          await page1.waitForFunction(() => {
-            const button = document.querySelector("#sfSaveASINsButton");
-            return (
-              button && button.textContent === "ASIN'leri SellerFlash'a Aktar"
-            );
-          });
-          // Click the button once it changes
-          await wait(1500);
-
-          await page1.click("#sfSaveASINsButton");
-          await wait(2000);
-
-          const sfSavingInfo = await page1.$("#sfSavingInfo");
-          const sfSavingInfoText = await page1.evaluate(
-            (element) => element.textContent,
-            sfSavingInfo
-          );
-
-          if (sfSavingInfoText.includes("ASIN listesi kaydedilemedi.")) {
-            console.error(`ASIN listesi kaydedilemedi. ${amazonLinks[i]}`);
-          }
-
-          await wait(900000); // orjinal version da 900000 e çıkarılacak
-          await page2.reload({ waitUntil: "networkidle0" });
-          await wait(2000);
-
-          await getInventoryTotal(page2);
-
-          remainingProducts = 49000 - totalInventory;
-          pagesToScan = Math.ceil(remainingProducts / 27);
-          if (pagesToScan <= 3) {
-            await page2.reload({ waitUntil: "networkidle0" });
-            await wait(3000);
-            continue;
-          }
-
-          if (pagesToScan > 400) {
-            pagesCanBeScanForFreeSpace = 400;
-          } else {
-            pagesCanBeScanForFreeSpace = pagesToScan;
-          }
-
-          pageCountMin = pageCountMax + 1;
-          pageCountMax = pageCountMax + pagesCanBeScanForFreeSpace;
-
-          if (pageCountMax >= totalPages) {
-            pageCountMax = totalPages;
-          }
+        if (pageCountMax >= totalPages) {
+          pageCountMax = totalPages;
         }
       }
+    }
+    // Uygulamanın durumunu izleme.
+    console.log(`${index}. amazonLink:`, amazonLink);
+    if (index === amazonLinks.length) {
+      console.log("Son eleman tamamlandı.");
     }
   }
 }
@@ -312,7 +327,7 @@ async function getInventoryTotal(page2) {
 
     let totalInventory =
       magazayaAktarilan + kuyrukta + onayBekleyen + kriterDisi;
-
+    console.log("totalInventory ayrı fonksiyondaki :>> ", totalInventory);
     return totalInventory;
   } catch (error) {
     console.log("Seçici bulunamadı: ", error);
